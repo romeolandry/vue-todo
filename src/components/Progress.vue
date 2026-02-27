f5ce427dda41e31e9a532db940616d9b40d2dfa4
Romeo Landry Kamgo Chetchom <kamgoche@yahoo.com>
2026-02-27 19:36:36 +0100

    change progress with ProgressBar

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,13 +1,36 @@
 <template>
-    <div>
-        Step {{ value + 1 }}/{{ max + 1 }}
-        <progress :value="value" :max="max"></progress>
+    <div class="progressbar-container">
+        <span>Step {{ value + 1 }}/{{ max + 1 }}</span>
+        <ProgressBar :value="progression"> {{ progression }}% </ProgressBar>
     </div>
 </template>
 
 <script setup>
-defineProps({
+
+import {ref,watch} from 'vue';
+import ProgressBar from 'primevue/progressbar';
+
+const progression = ref(null);
+
+const props = defineProps({
     value: Number,
     max: Number
 });
+
+watch(() => props.value, () => {
+    progression.value = (props.value / props.max) * 100;
+});
+
+
 </script>
+
+<style scoped>
+.progressbar-container {
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+}
+</style>
